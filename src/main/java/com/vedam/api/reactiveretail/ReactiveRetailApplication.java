0bd4e48237f44547697fac7b0b1eb043e2e666e3
praveenbandi <ipraveenbandi@gmail.com>
2021-03-30 00:08:06 -0400

    adding files

---
+++ src/main/java/com/vedam/api/reactiveretail/ReactiveRetailApplication.java
@@ -0,0 +1,13 @@
+package com.vedam.api.reactiveretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveRetailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vedam/api/reactiveretail/ReactiveRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedam.api.reactiveretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRetailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
